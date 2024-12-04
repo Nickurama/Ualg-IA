28795a1f8e99db30b761450176c434792206f880
Luna <rokdoombeast@gmail.com>
2024-12-04 10:25:33 +0000

    started data preprocessing

---
+++ lab5/src/main/DataPreprocessor.java
@@ -0,0 +1,21 @@
+/**
+ * Responsible for processing data in an AI context.
+ * Has several utility methods for data preprocessing.
+ */
+public class DataPreprocessor
+{
+	/**
+	 * normalizes data from a file, and places it onto another file
+	 * @pre needs the file to be in CSV format.
+	 * @param dataFile the file to read the data from
+	 * @param normalizedFile the file to write the normalized data to
+	 * @param dataMinValue the minimum value in the data set
+	 * @param dataMaxValue the maximum value in the data set
+	 * @param minClampValue the minimum value to normalize to
+	 * @param maxClampValue the maximum value to normalize to
+	 */
+	public static void normalize(String dataFile, String normalizedFile, double dataMinValue, double dataMaxValue, double minClampValue, double maxClampValue)
+	{
+
+	}
+}
---
+++ lab5/todo.md
@@ -0,0 +1,9 @@
+# TODO
+- O make normalization function, that reads from file, normalizes and then writes to file.
+- O make an exporting and importing function for neural networks.
+- O make a function that randomizes the input lines
+- O make an analysis of the network using random samples as training/testing and check testing values
+
+- O implement momenturm term ??
+
+- O do report
